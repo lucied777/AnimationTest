d3cb2f13f93f7a4e9667fd824ac540cef08af139
lucied <larzulu3@yahoo.co.jp>
2021-12-20 12:34:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/animationtest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.animationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnAnimationButton(View view){
+        float y = view.getY() + 500;
+        view.animate().setDuration(1000).setInterpolator(new BounceInterpolator()).y(y);
+    }
+}
